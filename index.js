eda7319fbae96110ec5109729d6db42c791c0a68
remalyaas <95465186+remalyaas@users.noreply.github.com>
2022-05-29 17:16:38 +0700

    update index + package

--- index.js
+++ index.js
@@ -4,23 +4,38 @@ import express from 'express';
 const PORT = 8080;
 const app = express();
 
-app.use(express.static(path.resolve('public')));
-app.set('view engine', 'ejs');
-app.set('views', './views');
 
-let current = '';
+app.set('view engine', 'ejs');
+app.set('views', './views/Page');
+
+app.use(express.static(path.resolve('public')));
 
 app.get('/', (req, res) => {
-    if(req.query.color !== undefined){
-        current = req.query.color;
-    }
-    res.render('loginpage',{
-        color: current
-    });
+    res.render('loginpage');
+});
+
+app.get('/menu', (req, res) => {
+    res.render('menu');
+});
+
+app.get('/periode', (req, res) => {
+    res.render('periode');
+});
+
+app.get('/dataMurid', (req, res) => {
+    res.render('dataMurid');
+});
+
+app.get('/dataGuru', (req, res) => {
+    res.render('dataGuru');
+});
+
+app.get('/daftarHadir', (req, res) => {
+    res.render('daftarHadir');
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is ready, listening on port ${PORT}`);
+    console.log('Server is ready, listening on port ${PORT}');
 });
 
 
