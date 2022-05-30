9bb3d94bd1beee469873df18ee26cf55ec88a737
remalyaas <95465186+remalyaas@users.noreply.github.com>
2022-05-30 20:03:41 +0700

    menu

---
+++ public/JS/menu.js
@@ -0,0 +1,12 @@
+function goToPeriode() {
+    window.location.href="/periode";  
+  }
+  function goToMurid() {
+    window.location.href="/dataMurid";  
+  }
+  function goToGuru() {
+    window.location.href="/dataGuru";  
+  }
+  function goToHadir() {
+    window.location.href="/daftarHadir";  
+  }
