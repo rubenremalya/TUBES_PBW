e7d86bd9e8e78c4b0ec8d3922207e9e52bd9ba68
remalyaas <95465186+remalyaas@users.noreply.github.com>
2022-06-05 18:43:47 +0700

    update index + login page

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ import * as url from 'url';
 
 import path from 'path';
 import express from 'express';
+import mysql from 'mysql';
 
 const PORT = 8080;
 const app = express();
-
 
 app.set('view engine', 'ejs');
 app.set('views', [path.join(__dirname, 'views'), 
@@ -16,8 +16,45 @@ path.join(__dirname, 'views/Admin')]);
 
 app.use('/public', express.static(path.resolve('public')));
 
-app.get('/', (req, res) => {
-    res.render('loginpage');
+const pool = mysql.createPool({
+    user: 'root',
+    password: '',
+    database: 'dtbs_PTMT',
+    host: 'localhost',
+    connectionLimit: 10
+});
+
+const dbConnect = () => {
+    return new Promise((resolve, reject) => {
+        pool.getConnection((err, conn) => {
+            if(err) {
+                reject(err);
+            } else{
+                resolve(conn);
+            }
+        })
+    })
+}
+
+const getUsers = conn => {
+    return new Promise((resolve, reject) => {
+        conn.query('SELECT username_admin FROM admin', (err, result) => {
+            if(err) {
+                reject(err);
+            } else{
+                resolve(result);
+            }
+        })
+    })
+}
+
+app.get('/', async (req, res) => {
+    const conn = await dbConnect();
+    const users = await getUsers(conn);
+    conn.release();
+    res.render('loginpage', {
+        users
+    });
 });
 
 app.get('/ChangePass', (req, res) => {
@@ -28,8 +65,13 @@ app.get('/menu', (req, res) => {
     res.render('menu');
 });
 
-app.get('/periode', (req, res) => {
-    res.render('periode');
+app.get('/periode', async (req, res) => {
+    const conn = await dbConnect();
+    const users = await getUsers(conn);
+    conn.release();
+    res.render('periode', {
+        users
+    });
 });
 
 app.get('/dataMurid', (req, res) => {
