9f1a619313f73a29b6d4b7b517d401bded07bebe
remalyaas <95465186+remalyaas@users.noreply.github.com>
2022-06-07 21:38:53 +0700

    update database

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(session({
 	saveUninitialized: true
 }));
 
-const pool = mysql.createPool({
+const connection = mysql.createConnection({
     user: 'root',
     password: '',
     database: 'dtbs_ptmt',
@@ -67,36 +67,7 @@ app.get('/', function (req, res) {
 	res.render('loginpage');
 });
 
-app.post('/auth', function(req, res) {
-	// Capture the input fields
-	let username = req.body.username;
-	let password = req.body.password;
-	// Ensure the input fields exists and are not empty
-	if (username && password) {
-		// Execute SQL query that'll select the account from the database based on the specified username and password
-		connection.query('SELECT * FROM admin WHERE username_admin = ? AND pass_admin = ?', 
-        [username, password], function(error, results, fields) {
-			// If there is an issue with the query, output the error
-			if (error) throw error;
-			// If the account exists
-			if (results.length > 0) {
-				// Authenticate the user
-				req.session.loggedin = true;
-				req.session.username = username;
-				// Redirect to home page
-				res.redirect('/menu');
-			} else {
-				res.send('Incorrect Username and/or Password!');
-			}			
-			res.end();
-		});
-	} else {
-		res.send('Please enter Username and Password!');
-		res.end();
-	}
-});
-
-app.get('/menu', async (req, res) => {
+app.get('/', async (req, res) => {
     const conn = await dbConnect();
     const users = await getUsers(conn);
     conn.release();
@@ -113,10 +84,40 @@ app.get('/menu', async (req, res) => {
     });
 });
 
+app.post('/auth', function(req, res) {
+	// Capture the input fields
+	let username = req.body.username;
+	let password = req.body.password;
+	// Ensure the input fields exists and are not empty
+	if (username && password) {
+		connection.query('SELECT * FROM admin WHERE username_admin = ? AND pass_admin = ?', [username, password], function(error, results, fields) {
+			// If there is an issue with the query, output the error
+			if (error) throw error;
+			// If the account exists
+			if (results.length > 0) {
+				// Authenticate the user
+				req.session.loggedin = true;
+				req.session.username = username;
+				// Redirect to home page
+				res.redirect('/menu');
+			} else {
+				res.redirect('/incorrect');
+			}			
+			res.end();
+		});
+	} else {
+		res.redirect('/');
+		res.end();
+	}
+});
+
 app.get('/ChangePass', (req, res) => {
     res.render('ChangePass');
 });
 
+app.get('/incorrect', (req, res) => {
+    res.render('incorrect');
+})
 
 //--------- ADMIN -------
 app.get('/menu', (req, res) => {
@@ -124,12 +125,7 @@ app.get('/menu', (req, res) => {
 });
 
 app.get('/periode', async (req, res) => {
-    const conn = await dbConnect();
-    const users = await getUsers(conn);
-    conn.release();
-    res.render('periode', {
-        users
-    });
+    res.render('periode')
 });
 
 app.get('/dataMurid', (req, res) => {
--- public/JS/login.js
+++ public/JS/login.js
@@ -1,3 +1,3 @@
 function goTo() {
-    window.location.href="/menu";  
+    window.location.href="/";  
   }
